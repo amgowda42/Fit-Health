1ec68639dd2053fbbcf7a20745a026f13f60093c
Annappa Gowda <annappag2020@gmail.com>
2024-02-01 01:36:08 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
-
-import { Testimonials, Footer, Header, SpecialServ } from "./container";
-import { Navbar } from "./components";
 import "./App.css";
 
 const App = () => (
   <div>
     <Navbar />
-    <Header />
-    <Testimonials />
-    <SpecialServ />
-    <Footer />
   </div>
 );
 
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import SubHeading from "./SubHeading/SubHeading";
-import FooterOverlay from "./Footer/FooterOverlay";
-import Services from "./Services/Services";
-import Navbar from "./Navbar/Navbar";
-
-export { SubHeading, FooterOverlay, Services, Navbar };
