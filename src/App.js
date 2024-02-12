8671c2f6eff876f152faf1aa18fe3c2d922bad13
Annappa Gowda <annappag2020@gmail.com>
2024-02-13 00:10:42 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+
+import { Testimonials, Footer, Header, SpecialServ } from "./container";
+import { Navbar } from "./components";
 import "./App.css";
 
 const App = () => (
   <div>
     <Navbar />
+    <Header />
+    <Testimonials />
+    <SpecialServ />
+    <Footer />
   </div>
 );
 
---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Header.css";
+import Form from "../../components/Form/Form";
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Fithealth for Goodhealth" />
+      <h1 className="app__header-h1">Please Book You Doctors </h1>
+      <Form />
+      <br />
+      <br />
+      <button type="button" className="custom__button">
+        <a href="#ser">Explore Services</a>
+      </button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header_img" />
+    </div>
+  </div>
+);
+
+export default Header;
